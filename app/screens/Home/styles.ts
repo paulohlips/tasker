1d61d0b5caee4487810a6ef601f81a4303832e1b
paulohlips <paulohfcmendes@gmail.com>
2024-07-09 11:21:09 -0300

    feat: creates basic home screen

---
+++ app/screens/Home/styles.ts
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  backgroud: {
+    flex: 1,
+    backgroundColor: '#262626'
+  },
+  superiorView: {
+    height: 173,
+    backgroundColor: '#0D0D0D'
+  }
+})
