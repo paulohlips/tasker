4912ca70e6ffbe369e854f3601c40280e4c0374b
paulohlips <paulohfcmendes@gmail.com>
2024-07-09 12:54:49 -0300

    feat: updates home screen

--- app/screens/Home/index.tsx
+++ app/screens/Home/index.tsx
@@ -1,9 +1,24 @@
-import { View } from "react-native";
+import { View, Image, Text } from "react-native";
 import { styles } from './styles'
+import { Header } from '../../components/InputBar'
 export function Home() {
   return (
-    <View style={styles.backgroud}>
-      <View style={styles.superiorView}></View>
+    <View style={styles.background}>
+      <View style={styles.superiorView}>
+        <Image
+          style={styles.rocket}
+          source={require('../../../assets/images/rocket.png')}
+        />
+        <View style={styles.title}>
+          <Text style={styles.tas}>
+            tas
+          </Text>
+          <Text style={styles.ker}>
+            ker
+          </Text>
+        </View>
+      </View>
+      <Header />
     </View>
   )
 }
--- app/screens/Home/styles.ts
+++ app/screens/Home/styles.ts
@@ -1,12 +1,36 @@
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-  backgroud: {
+  background: {
     flex: 1,
-    backgroundColor: '#262626'
+    backgroundColor: '#1A1A1A'
   },
   superiorView: {
     height: 173,
-    backgroundColor: '#0D0D0D'
+    backgroundColor: '#0D0D0D',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row'
+  },
+  rocket: {
+    height: 32,
+    width: 20,
+    color: '#4EA8DE',
+    marginRight: 5
+  },
+  title: {
+    flexDirection: 'row',
+  },
+  tas: {
+    color: '#4EA8DE',
+    fontSize: 28,
+    fontWeight: 'bold',
+    marginBottom: 10
+  },
+  ker: {
+    color: '#8284FA',
+    fontSize: 28,
+    fontWeight: 'bold',
+    marginBottom: 10
   }
 })
