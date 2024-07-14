7e9b3760a8e6159d0eab756825ad6d9ab78cfd90
paulohlips <paulohfcmendes@gmail.com>
2024-07-14 14:33:49 -0300

    feat: creates Task component

--- app/components/Task/index.tsx
+++ app/components/Task/index.tsx
@@ -1,12 +1,15 @@
 import { Image, View, Text } from "react-native";
 import { CheckBox } from "../CheckBox";
-
+import { styles } from './styles'
 export function Task() {
   return (
-    <View>
+    <View style={styles.container}>
       <CheckBox />
-      <Text>My task</Text>
-      <Image />
+      <Text style={styles.text}>Integer urna interdum massa libero auctor neque turpis turpis semper.</Text>
+      <Image
+        style={styles.garbageIcon}
+        source={require('../../../assets/images/garbage.png')}
+      />
     </View>
   )
 }
---
+++ app/components/Task/styles.ts
@@ -0,0 +1,24 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#262626",
+    height: 64,
+    marginHorizontal: 24,
+    marginVertical: 8
+  },
+  text: {
+    flex: 1,
+    color: "#F2F2F2",
+    marginLeft: 8,
+    width: 40,
+    fontSize: 14
+  },
+  garbageIcon: {
+    height: 16,
+    width: 16,
+    marginHorizontal: 8
+  }
+})
